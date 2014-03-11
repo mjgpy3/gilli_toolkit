1b54630f37a3dd6f6d0ca3ec2daac1305595b284
Michael Gilliland <mjg.py3@gmail.com>
2014-03-10 22:53:35 -0400

    Added find and replace

--- src/gilli_toolkit.py
+++ src/gilli_toolkit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from sys import argv
+from os import system
 import json
 
 class JsonTools(object):
@@ -29,6 +30,15 @@ class JsonTools(object):
             with open(file, 'r') as f:
                 print eval(json.dumps(f.read(), sort_keys=True, indent=4, separators=(',', ': ')))
 
+class Directory(object):
+    def find_and_replace(self, args):
+        directory = args[2]
+        file_extension = args[3]
+        to_replace = args[4]
+        replacement = args[5]
+
+        system('find ' + directory + ' -name "*.' + file_extension +'" | xargs sed -i "s/' + to_replace + '/' + replacement + '/g"')
+
 if __name__ == '__main__':
     if argv[1:3] == ['json', 'valid?']:
         print "Validating json for", str(argv[3:]), '\n'
@@ -36,8 +46,13 @@ if __name__ == '__main__':
     elif argv[1:3] == ['json', 'pretty']:
         print "Prettying json", str(argv[3:]), '\n'
         JsonTools().pretty(argv[3:])
+    elif argv[1] == 'far':
+        print "At '" + argv[2] + "' in files '" + argv[3] + "':", argv[4], " -> ", argv[5]
+        Directory().find_and_replace(argv)
     else:
         print "Usage: gilli [options]"
         print "\nJSON:"
         print "    json valid? [files] - validate some json files"
         print "    json pretty [files] - pretty print some json files"
+        print "\nDirectories"
+        print "    far [dir] [extension] [old] [new] - recursively find and replace"
